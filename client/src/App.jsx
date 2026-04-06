1058dbb6e321c5019a6d1a78573380bc89a646f7
Yuvashree120 <yuvashree.k007@gmail.com>
2026-04-06 10:52:00 +0530

    client is commited

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white">
+      <h1 className="text-5xl font-semibold text-pink-500">TaskFlow</h1>
+    </div>
+  )
+}
+
+export default App
